9de47622d726f4de56a646ccb8d314b5a01f244c
montyplexer <montague.7676@gmail.com>
2022-08-08 00:57:54 -0400

    Started work on Task#2, 'create' REST method

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -1,12 +1,13 @@
 package com.starter.fullstack.rest;
 
 import com.starter.fullstack.api.Inventory;
+import com.starter.fullstack.api.Product;
 import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * Inventory Controller.
@@ -26,12 +27,36 @@ public class InventoryController {
   }
 
   /**
-   * Find Products.
+   * Find Inventories.
    * @return List of Product.
    */
   @GetMapping
   public List<Inventory> findInventories() {
     return this.inventoryDAO.findAll();
   }
+
+  /** TODO */
+
+  /**
+   * Save Inventory.
+   * @param inventory inventory.
+   * @return Inventory.
+   */
+  @PostMapping("/inventory")
+  public Product saveInventory(@Valid @RequestBody Inventory inventory) {
+    return this.inventoryDAO.save(inventory);
+  }
+
+  /**
+   * Delete Product By Id.
+   *
+   * @param ids ids.
+   */
+  @DeleteMapping("/inventory")
+  public void deleteProductById(@RequestBody List<String> ids) {
+    Assert.notEmpty(ids, "Product Ids were not provided");
+    this.productDAO.deleteProductsByIdIn(ids);
+  }
+
 }
 
